53b21ce8fb21cba280d54c0d15d01ed7bb588e70
BaezCrdrm <dev.baez_crdrm@outlook.com>
2016-06-06 14:54:25 -0500

    Alta invitados

--- admin/index.php
+++ admin/index.php
@@ -10,18 +10,30 @@
     <head>
         <meta charset="utf-8"/>
         <title>Admin</title>  
+        
+        <script type="text/javascript">            
+            function fecha()
+            {
+                var d = new Date();                    
+                document.getElementById("hiDia").value = d.getDate();
+                document.getElementById("hiMes").value = d.getDate();
+            }
+        </script>
     </head>
     
-    <body>
-        <form id="alta" action="../scripts/alta">
+    <body onload="fecha()">
+        <form id="alta" action="../scripts/alta.php">
             <label for="">Individuo</label> <input type="radio" name="sel1" checked="checked" value="individuo" onchange="cambioPersona('textP1', 'textP2')"/><br>
             <input type="text" name="nombre" class="textP1" placeholder="Nombre de persona" /><br>
             <input type="text" name="apellido" class="textP1" placeholder="Apellido de persona" /><br><br>
             
             <label for="">Familia</label> <input type="radio" name="sel1" value="grupo" onchange="cambioPersona('textP2', 'textP1')"/><br>
             <input type="text" name="grupo" class="textP2" placeholder="Familia/Grupo" disabled="disabled"/><br>
-            <input type="number" name="cantGrupo" class="textP2" placeholder="Integrantes" disabled="disabled"/><br>
-            <input type="number" name="cantFamNinos" class="textP2" placeholder="Niños" disabled="disabled"/><br><br>
+            <input type="number" name="cantGrupo" min="2" class="textP2" placeholder="Integrantes" disabled="disabled"/><br>
+            <input type="number" name="cantFamNinos" min="0" class="textP2" placeholder="Niños" disabled="disabled" value = "0"/><br><br>
+            
+            <input type="hidden" name="mes" id="hiMes"/>
+            <input type="hidden" name="dia" id="hiDia"/>            
             
             <input type="submit" value="Agregar"/>
         </form>        
--- scripts/consulta.php
+++ scripts/consulta.php
@@ -1,3 +1,8 @@
 <?php
-    
+    function genQuery($query)
+    {
+        require "conexion.php";
+        $consulta = mysqli_query($conecta, $query);
+        return $consulta;
+    }
 ?>
