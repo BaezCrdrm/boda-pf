5c9e81ca940b7ef591b95086676f5dd5c09c2a95
BaezCrdrm <dev.baez_crdrm@outlook.com>
2016-06-25 21:39:27 -0500

    Cambios generales
    Ya casi!

--- scripts/easteregg.js
+++ scripts/easteregg.js
@@ -1,3 +1,10 @@
 function merry(h1) {
     h1.innerHTML = "Will you merry me?...<br> I wanna wish you a Merry Christmas";
+    audioPlayer("../multimedia/mc.mp3");
+}
+
+function audioPlayer(file) {
+    var hs = document.getElementById("hs");
+    hs.src = file;
+    hs.play();
 }
