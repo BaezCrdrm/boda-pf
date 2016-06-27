7de80105f9ff6cb9e27df2934c747b2537fd1ad9
BaezCrdrm <dev.baez_crdrm@outlook.com>
2016-06-27 02:26:40 -0500

    Pase

--- pase/mi-pase/index.php
+++ pase/mi-pase/index.php
@@ -11,10 +11,75 @@
         <link rel="stylesheet" href="../../CSS/General.css" media="all">
         <link rel="stylesheet" href="../../CSS/PasePrint.css" media="print">
         <link rel="stylesheet" href="../../CSS/Pase.css" media="all">
+        <link rel="stylesheet" href="../../CSS/PaseScreen.css" media="screen">
     </head>
 
     <body>
         <div id="pase">
+            <img src=<?php
+                switch ($_SESSION['tipo']) {
+                    case "ind":
+                        echo "'../../img/pase/Pase-personal.jpg'";
+                        break;
+
+                    case "fam":
+                        echo "'../../img/pase/Pase-adultos_infante.jpg'";
+                        break;
+
+                    case "gr":
+                        echo "'../../img/pase/Pase-adultos.jpg'";
+                        break;
+                }
+            ?>>
+
+            <div id="contenedor-nombre">
+                <p id="nombre"><?php
+                    switch ($_SESSION['tipo']) {
+                        case "ind":
+                            $name = $_SESSION['nombre']." ".$_SESSION['apellido'];
+                            if(count($name) < 28) {
+                                echo $name;
+                            } else {
+                                echo $_SESSION['nombre']."<br>".$_SESSION['apellido'];
+                            }
+                            break;
+
+                        case "fam":
+                            echo "Fam. ".$_SESSION['nombre'];
+                            break;
+
+                        case "gr":
+                            echo $_SESSION['nombre'];
+                            break;
+                    }
+                ?></p>
+            </div>
+
+            <div class="contenedores" id="contenedor-adultos">
+                <p id="adultos"> <?php
+                    if(!isset($_SESSION['ninios'])) {
+                        echo "<style>
+                            #contenedor-adultos {
+                                top: 730px !important;
+                            }
+                        </style>";
+                    }
+
+                    if(isset($_SESSION['cantidad'])) {
+                        echo $_SESSION['cantidad'];
+                    }
+                ?>
+                </p>
+            </div>
+
+            <div class="contenedores" id="contenedor-infantes">
+                <p id="inf"> <?php
+                    if(isset($_SESSION['ninios'])) {
+                        echo $_SESSION['ninios'];
+                    }
+                ?>
+                </p>
+            </div>
         </div>
 
         <div id="div-formC" title="El código ha sido proporcionado de manera única.">
